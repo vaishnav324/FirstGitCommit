24d80dc3a569bcefe397f38d0cb35cd34d9a1f1f
vaishnav324 <s572259@nwmissouri.edu>
2024-01-24 20:51:53 -0600

    First Commit

---
+++ GitCommit/src/gitcommit/GitCommit.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gitcommit;
+
+/**
+ *
+ * @author S572259
+ */
+public class GitCommit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        // TODO code application logic here
+    }
+    
+}
